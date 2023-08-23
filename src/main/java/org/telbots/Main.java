8a17daaf928a9f5a8aa339b6d585dba305b08956
dileep98 <dk26092@gmail.com>
2023-08-23 17:03:06 -0400

    Initial changes

---
+++ src/main/java/org/telbots/Main.java
@@ -0,0 +1,21 @@
+package org.telbots;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new MyBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/telbots/MyBot.java
@@ -0,0 +1,40 @@
+package org.telbots;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyBot extends TelegramLongPollingBot {
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // TODO
+        return "git_folder_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // TODO
+        return "6559941276:AAEXfjqXu2N-g5ZZon5H3J32sJV3svBIwv4";
+    }
+
+
+}
